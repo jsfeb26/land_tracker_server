d6f74386b2e1fa083a12b3f871cd5382ae7e8052
Jason Stinson <jstinson@atlassian.com>
2018-08-07 08:44:27 -0400

    add passport and google strategy

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-// route handler for get request to '/'
-app.get("/", (req, res) => {
-  res.send({ hi: "poop master" });
-});
+// new GoogleStrategy() creates new instance of GoogleStrategy
+// tell passport about new strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log(refreshToken);
+      console.log(profile);
+    }
+  )
+);
+
+// route handler for /auth/google
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+// route handler for /auth/google/callback
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 // check environment to see if PORT is set
 const PORT = process.env.PORT || 5000;
