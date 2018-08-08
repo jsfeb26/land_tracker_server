bdc5bcbcf07b5715dd5093c31a8f16995ba76e8c
Jason Stinson <jstinson@atlassian.com>
2018-08-08 09:19:04 -0400

    configure using cookies to authenticate users

--- services/passport.js
+++ services/passport.js
@@ -8,6 +8,15 @@ const keys = require("../config/keys");
 // required multiple times
 const User = mongoose.model("users"); // get User Model
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+
 // new GoogleStrategy() creates new instance of GoogleStrategy
 // tell passport about new strategy
 passport.use(
