4a907dc15d5e51488e17fb3e46c2de36a3919849
Jason Stinson <jstinson@atlassian.com>
2018-08-08 09:26:07 -0400

    add routes for checking user and logging out

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,4 +11,15 @@ module.exports = app => {
 
   // route handler for /auth/google/callback
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  // route handler for /api/
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  // route handler for /api/current_user
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 };
