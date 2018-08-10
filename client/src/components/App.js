87c3747c9ca522a353f98f5318772d87b28c712d
Jason Stinson <jstinson@atlassian.com>
2018-08-10 08:10:06 -0400

    add rough routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const NewMailing = () => <h2>New Mailing</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/mailings" component={Dashboard} />
+          <Route path="/mailings/new" component={NewMailing} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
