c259ae49fea1f7de25320f02d939ebe88bce96a4
Jason Stinson <jstinson@atlassian.com>
2018-08-11 08:43:52 -0400

    add rough header component and materialize-css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
-const Header = () => <h2>Header</h2>;
+import Header from "./Header";
 const Dashboard = () => <h2>Dashboard</h2>;
 const NewMailing = () => <h2>New Mailing</h2>;
 const Landing = () => <h2>Landing</h2>;
