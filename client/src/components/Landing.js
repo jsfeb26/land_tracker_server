89a70fdc1349425637b3025ed43fd83bad10a7e4
Jason Stinson <jstinson@atlassian.com>
2018-08-11 10:58:39 -0400

    finish up the header routing

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Land Tracker!</h1>
+      Track everything in your land business
+    </div>
+  );
+};
+
+export default Landing;
