44b1b17b4451a0b2cdfae1e88c945d209d68d2a9
Jason Stinson <jstinson@atlassian.com>
2018-08-11 18:54:25 -0400

    add credits to user and require authentication for stripe middleware

---
+++ middlewares/requireLogin.js
@@ -0,0 +1,10 @@
+// next is a function that we call when function is complete
+// next allows you to pass request off to next middleware or function in chain
+module.exports = (req, res, next) => {
+  if (!req.user) {
+    return res.status(401).send({ error: "You must log in!" });
+    // by not calling next we're stopping this request here
+  }
+
+  next();
+};
