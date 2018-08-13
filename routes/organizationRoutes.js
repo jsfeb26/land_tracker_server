7d8ea209284a8736d25357046ae00a9ad974ce1c
Jason Stinson <jstinson@atlassian.com>
2018-08-12 21:04:14 -0400

    add organization routes

--- routes/organizationRoutes.js
+++ routes/organizationRoutes.js
@@ -1,1 +1,45 @@
-module.exports = () => {};
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+
+const Organization = mongoose.model("organizations");
+
+module.exports = app => {
+  app.post("/api/organization", requireLogin, async (req, res) => {
+    const {
+      name,
+      address,
+      city,
+      state,
+      zipCode,
+      phone,
+      fax,
+      email,
+      deedTitling,
+      deedType,
+      docFee,
+      lateFee,
+      noteServicingFee,
+      gracePeriod
+    } = req.body;
+
+    const newOrganization = new Organization({
+      name,
+      address,
+      city,
+      state,
+      zipCode,
+      phone,
+      fax,
+      email,
+      deedTitling,
+      deedType,
+      docFee,
+      lateFee,
+      noteServicingFee,
+      gracePeriod,
+      users: [req.user]
+    });
+
+    await newOrganization.save();
+  });
+};
