cd3a094f33dcad391360edfee340d3ae878f2d23
Jason Stinson <jstinson@atlassian.com>
2018-08-21 09:21:43 -0400

    add lob configs

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,8 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  lobPublishableKey: process.env.LOB_PUBLISHABLE_KEY,
+  lobSecretKey: process.env.LOB_SECRET_KEY,
+  lobOfferLetterTemplate: process.env.LOB_OFFER_LETTER_TEMPLATE
 };
