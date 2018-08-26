13e6685349fb06f3007b4412c18dc05c83e2a84d
Jason Stinson <jstinson@atlassian.com>
2018-08-26 18:59:17 -0400

    update styling for header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,16 +2,11 @@ import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-
-import Payments from "./Payments";
 
 const styles = {
   root: {
@@ -23,10 +18,26 @@ const styles = {
   menuButton: {
     marginLeft: -12,
     marginRight: 20
+  },
+  appBar: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  toolBar: {
+    flexGrow: 1,
+    padding: 0
   }
 };
 
 class Header extends Component {
+  HeaderLink = props => {
+    return <Link to={this.props.auth ? "/dashboard" : "/"} {...props} />;
+  };
+  DashboardLink = props => {
+    return <Link to={"/dashboard"} {...props} />;
+  };
+  LogoIcon = props => <img src="https://image.flaticon.com/icons/svg/888/888568.svg" />;
   renderContent() {
     switch (this.props.auth) {
       case null:
@@ -36,10 +47,10 @@ class Header extends Component {
       default:
         return (
           <Fragment>
-            <Button href="/dashboard">Dashboard</Button>
+            <Button component={this.DashboardLink}>Dashboard</Button>
             <Button href="/parcels">Parcels</Button>
             <Button href="/parcel/upload">Upload Parcels</Button>
-            <Button href="/api/logout" variant="outlined" color="secondary">
+            <Button href="/api/logout" variant="outlined" color="default">
               Logout
             </Button>
           </Fragment>
@@ -57,29 +68,17 @@ class Header extends Component {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="title" color="inherit" className={classes.flex}>
-              <Link to={this.props.auth ? "/dashboard" : "/"} className="left brand-logo">
-                Land Tracker
-              </Link>
-            </Typography>
-            {this.renderContent()}
-          </Toolbar>
+        <AppBar className={classes.appBar} position="static">
+          <div className="toolbar-body">
+            <Toolbar className={classes.toolBar}>
+              <Typography variant="title" className={classes.flex} component={this.HeaderLink}>
+                LandTrackr
+              </Typography>
+              {this.renderContent()}
+            </Toolbar>
+          </div>
         </AppBar>
       </div>
-
-      // <nav>
-      //   <div className="nav-wrapper">
-      //     <Link to={this.props.auth ? "/dashboard" : "/"} className="left brand-logo">
-      //       Land Tracker
-      //     </Link>
-      //     <ul className="right">{this.renderContent()}</ul>
-      //   </div>
-      // </nav>
     );
   }
 }
