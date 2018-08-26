f8ed01a6d657c0daa2dad310aed90fbba627b5a3
Jason Stinson <jstinson@atlassian.com>
2018-08-25 21:36:13 -0400

    update header to use material-ui

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,30 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
 import Payments from "./Payments";
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  flex: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
 
 class Header extends Component {
   renderContent() {
@@ -9,36 +32,54 @@ class Header extends Component {
       case null:
         return;
       case false:
-        return (
-          <li>
-            <a href="/auth/google">Login With Google</a>
-          </li>
-        );
+        return <Button href="/auth/google">Login With Google</Button>;
       default:
         return (
           <Fragment>
-            <li>
-              <Payments />
-            </li>
-            <li style={{ margin: "0 10px" }}>Credits: {this.props.auth.credits}</li>
-            <li>
-              <a href="/api/logout">Logout</a>
-            </li>
+            <Button href="/dashboard">Dashboard</Button>
+            <Button href="/parcels">Parcels</Button>
+            <Button href="/parcel/upload">Upload Parcels</Button>
+            <Button href="/api/logout" variant="outlined" color="secondary">
+              Logout
+            </Button>
           </Fragment>
         );
+      // <Fragment>
+      //   {/* <li>
+      //     <Payments />
+      //   </li>
+      //   <li style={{ margin: "0 10px" }}>Credits: {this.props.auth.credits}</li> */}
+      // </Fragment>
     }
   }
 
   render() {
+    const { classes } = this.props;
     return (
-      <nav>
-        <div className="nav-wrapper">
-          <Link to={this.props.auth ? "/dashboard" : "/"} className="left brand-logo">
-            Land Tracker
-          </Link>
-          <ul className="right">{this.renderContent()}</ul>
-        </div>
-      </nav>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              <Link to={this.props.auth ? "/dashboard" : "/"} className="left brand-logo">
+                Land Tracker
+              </Link>
+            </Typography>
+            {this.renderContent()}
+          </Toolbar>
+        </AppBar>
+      </div>
+
+      // <nav>
+      //   <div className="nav-wrapper">
+      //     <Link to={this.props.auth ? "/dashboard" : "/"} className="left brand-logo">
+      //       Land Tracker
+      //     </Link>
+      //     <ul className="right">{this.renderContent()}</ul>
+      //   </div>
+      // </nav>
     );
   }
 }
@@ -47,4 +88,4 @@ function mapStateToProps({ auth }) {
   return { auth };
 }
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(withStyles(styles)(Header));
