1d0bf2a060b1c8df9e5f609adfeb3ebbc0d38f38
Jason Stinson <jstinson@atlassian.com>
2018-09-03 14:41:38 -0400

    move orgParcel stuff into parcelReducer

--- client/src/reducers/orgReducer.js
+++ client/src/reducers/orgReducer.js
@@ -2,9 +2,7 @@ import {
   CREATING_ORGANIZATION,
   CREATED_ORGANIZATION,
   FETCHING_USER_ORGS,
-  FETCH_USER_ORGS,
-  FETCHING_ORG_PARCELS,
-  FETCH_ORG_PARCELS
+  FETCH_USER_ORGS
 } from "../actions/types";
 
 export default function(state = {}, action) {
@@ -17,8 +15,6 @@ export default function(state = {}, action) {
       return { ...state, fetchingUserOrgs: true };
     case FETCH_USER_ORGS:
       return { ...state, fetchingUserOrgs: false, userOrgs: action.payload };
-    case FETCH_ORG_PARCELS:
-      return { ...state, orgParcels: action.payload };
     default:
       return state;
   }
--- client/src/reducers/parcelReducer.js
+++ client/src/reducers/parcelReducer.js
@@ -1,11 +1,37 @@
-import { SENDING_LETTER, SENT_LETTER } from "../actions/types";
+import { SENDING_LETTER, SENT_LETTER, FETCH_ORG_PARCELS } from "../actions/types";
+import get from "lodash.get";
 
-export default function(state = {}, action) {
+export default function(state = { orgParcels: [], sendingParcels: {} }, action) {
+  const id = get(action, "payload.id");
+
   switch (action.type) {
+    case FETCH_ORG_PARCELS:
+      return { ...state, ...{ orgParcels: action.payload } };
+
     case SENDING_LETTER:
-      return { ...state, creating: true };
+      return {
+        ...state,
+        ...{ ...state.sendingParcels, sendingParcels: { [id]: true } }
+      };
+
     case SENT_LETTER:
-      return { ...state, creating: false, created: true };
+      const orgParcels = state.orgParcels.map(parcel => {
+        if (parcel._id !== id) {
+          return parcel;
+        }
+
+        return {
+          ...parcel,
+          ...action.payload.data
+        };
+      });
+
+      return {
+        ...state,
+        ...{ orgParcels },
+        ...{ ...state.sendingParcels, sendingParcels: { [id]: false } }
+      };
+
     default:
       return state;
   }
