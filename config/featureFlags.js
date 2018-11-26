8ec79b5e4b8cfdd0bfc5d1ebf7d383bc045260c6
Jason Stinson <jsfeb26@gmail.com>
2018-11-25 21:16:48 -0500

    add feature flags

---
+++ config/featureFlags.js
@@ -0,0 +1,3 @@
+module.exports = {
+  requireCredits: false
+};
--- middlewares/requireCredits.js
+++ middlewares/requireCredits.js
@@ -1,7 +1,9 @@
+const featureFlags = require("../config/featureFlags");
+
 // next is a function that we call when function is complete
 // next allows you to pass request off to next middleware or function in chain
 module.exports = (req, res, next) => {
-  if (req.user.credits < 1) {
+  if (featureFlags.requireCredits && req.user.credits < 1) {
     return res.status(403).send({ error: "Not enough credits!" });
     // by not calling next we're stopping this request here
   }
